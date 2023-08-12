fc66c68570e58ece9f0d9df10741903de4942a72
izzet <izzetcetinkaya@gmail.com>
2023-08-13 00:26:06 +0200

    first commit

--- src/test/java/tests/HolaFlyApi.java
+++ src/test/java/tests/HolaFlyApi.java
@@ -1,8 +1,9 @@
-package get_method;
+package tests;
 
 import io.restassured.RestAssured;
 import io.restassured.path.json.JsonPath;
 import io.restassured.response.Response;
+import org.junit.Assert;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
@@ -20,62 +21,53 @@ public class HolaFlyApi {
     @Test
     public void getPeople() {
         Response response = RestAssured.get("people/1/");
-        response.then().statusCode(200);
 
-        JsonPath jsonPath=response.jsonPath();
-        System.out.println(jsonPath.getString("name"));
-        String masa=jsonPath.getString("mass");
-        System.out.println(jsonPath.getString("height"));
-        System.out.println(jsonPath.getString("homeworld"));
+        Assert.assertEquals("statusCode not match with expected", 200, response.statusCode());
+        Assert.assertEquals("people name not match with expected", "Luke Skywalker", response.path("name"));
+        Assert.assertEquals("people mass not match with expected", "77", response.path("mass"));
+        Assert.assertEquals("people height not match with expected", "172", response.path("height"));
 
+        String planet1EndPoint = response.path("homeworld").toString().substring(22);
+        Response homeworldResponse = RestAssured.get(planet1EndPoint);
 
-        String planetEndPoint= jsonPath.getString("homeworld").substring(22);
-        Response homeworld = RestAssured.get(planetEndPoint);
-        response.then().statusCode(200);
-        homeworld.prettyPrint();
-        String homeworldName = homeworld.path("name");
-        String homeworldId= planetEndPoint.substring(planetEndPoint.length()-2, planetEndPoint.length()-1);
-        System.out.println(homeworldName + " " + homeworldId);
+        String homeworldName = homeworldResponse.path("name");
+        String homeworldId = planet1EndPoint.substring(planet1EndPoint.length() - 2, planet1EndPoint.length() - 1);
 
-        String gravedad = homeworld.path("gravity").toString().substring(0,1);
+        Assert.assertEquals("homeworldName not match with expected", "Tatooine", homeworldName);
+        Assert.assertEquals("homeworldId not match with expected", "1", homeworldId);
     }
 
     @Test
     public void getPlanet() {
         Response response = RestAssured.get("planets/1/");
-        response.then().statusCode(200);
-        JsonPath jsonPath=response.jsonPath();
-        System.out.println(jsonPath.getString("name"));
-        String gravedad=jsonPath.getString("gravity");
 
+        JsonPath jsonPath = response.jsonPath();
+        Assert.assertEquals("statusCode not match with expected", 200, response.statusCode());
+        Assert.assertEquals("Planet name not match with expected", "Tatooine", jsonPath.getString("name"));
+        Assert.assertEquals("Planet gravity not match with expected", "1 standard", jsonPath.getString("gravity"));
     }
 
     @Test
-    public void peso(){
+    public void calculoPesoPersonaje() {
         Random rnd = new Random();
-       int randomId=  rnd.nextInt(82)+1;
+        int randomId = rnd.nextInt(82) + 1;
 
-        Response response = RestAssured.get("people/"+randomId+"/");
-        response.then().statusCode(200);
+        Response peopleResponse = RestAssured.get("people/" + randomId + "/");
+        peopleResponse.then().statusCode(200);
 
-        response.prettyPrint();
+        String planetEndPoint = peopleResponse.path("homeworld").toString().substring(22);
 
-        String masa=response.path("mass");
+        Response homeworldResponse = RestAssured.get(planetEndPoint);
+        homeworldResponse.then().statusCode(200);
 
+        String masaPersonaje = peopleResponse.path("mass");
+        String gravedadPlaneta = homeworldResponse.path("gravity").toString().substring(0, 1);
 
-
-        String planetEndPoint= response.path("homeworld").toString().substring(22);
-        Response homeworld = RestAssured.get(planetEndPoint);
-        response.then().statusCode(200);
-
-        String gravedad = homeworld.path("gravity").toString().substring(0,1);
-        homeworld.prettyPrint();
-if(masa.equals("unknown")||gravedad.equals("unknown") ) {
-    System.out.println("masa or gravedad is unknown");
-}else {
-    int Peso_Personaje = Integer.parseInt(masa) * Integer.parseInt(gravedad);
-    System.out.println("Peso_Personaje = " + Peso_Personaje);
-}
+        if (masaPersonaje.equals("unknown") || gravedadPlaneta.equals("unknown")) {
+            System.out.println("masaPersonaje or gravedadPlaneta is unknown");
+        } else {
+            int PesoPersonaje = Integer.parseInt(masaPersonaje) * Integer.parseInt(gravedadPlaneta);
+            System.out.println("Peso_Personaje = " + PesoPersonaje);
+        }
     }
-
 }
